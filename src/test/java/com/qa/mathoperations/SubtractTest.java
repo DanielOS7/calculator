4882b2b4a5298d526b29e66283dc8a2e3794b21b
Daniel Olugbade <Daniel.Olugbade@academytrainee.com>
2018-05-17 12:04:43 +0100

    Multiply feature added.

--- src/test/java/com/qa/mathoperations/SubtractTest.java
+++ src/test/java/com/qa/mathoperations/SubtractTest.java
@@ -1,20 +1,16 @@
 package com.qa.mathoperations;
 
-
 import org.junit.Assert;
-import org.junit.Test;
-
-
+import org.junit.Test; 
 
 public class SubtractTest {
-
+	
 	@Test
 	public void test() {
-		
 		Subtractor subtractor = new Subtractor();
 		int actual = subtractor.subtract(4,2);
 		Assert.assertEquals(2, actual);
 		
 	}
-
+	
 }

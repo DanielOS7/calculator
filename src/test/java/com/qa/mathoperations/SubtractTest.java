2972030fc44082f01d1b323221fc3fafd98f8f65
Daniel Olugbade <Daniel.Olugbade@academytrainee.com>
2018-05-17 11:46:08 +0100

    Subtract feature added.

---
+++ src/test/java/com/qa/mathoperations/SubtractTest.java
@@ -0,0 +1,20 @@
+package com.qa.mathoperations;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class SubtractTest {
+
+	@Test
+	public void test() {
+		
+		Subtractor subtractor = new Subtractor();
+		int actual = subtractor.subtract(4,2);
+		Assert.assertEquals(2, actual);
+		
+	}
+
+}
